036259182c5a2469036171b27585e6dc1b933c39
connorjennison <connorjennison@me.com>
2019-10-10 22:53:28 -0400

    Revert "Blah"
    This reverts commit 7e0c1e4c8916d5cbe4194a7be33133fe08fda316.

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,60 @@
+class TodosController < ApplicationController
+	before_action :set_todo, only: [:edit, :update, :show, :destroy]
+	def new
+		@todo = Todo.new
+	end
+	
+	
+	def create
+		@todo = Todo.new(todo_params)
+		if @todo.save
+			flash[:notice] = "Todo was created successfully!"
+			redirect_to todo_path(@todo)
+		else
+			render 'new'
+		end
+	end
+	
+	
+	def show
+	end
+	
+	
+	def edit
+	end
+	
+	
+	def update
+		if @todo.update(todo_params)
+			flash[:notice] = "Todo was successfully updated"
+			redirect_to todo_path(@todo)
+		else	
+			render 'edit'
+		end
+	end
+	
+	def destroy
+		if @todo.destroy
+			flash[:notice] = "Todo was deleted successfully"
+			redirect_to todos_path
+		else
+			render 'index'
+		end
+	end
+	
+	
+	def index
+		@todos = Todo.all
+	end
+	
+	
+	private
+	
+	def todo_params
+		params.require(:todo).permit(:name, :description)
+	end
+	
+	def set_todo
+		@todo = Todo.find(params[:id])
+	end
+end
