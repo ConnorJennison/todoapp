7e0c1e4c8916d5cbe4194a7be33133fe08fda316
connorjennison <connorjennison@me.com>
2019-10-10 22:49:22 -0400

    Blah

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  root "todos#index"
-	get "/about", to: "pages#about"
-	get "/help", to: "pages#help"# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	
-	resources :todos
-end
